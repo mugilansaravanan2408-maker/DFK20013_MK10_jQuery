9350dd66574244de223494d3f6e9bb3711fdbfab
Your Full Name <yourgithubemail@gmail.com>
2026-03-16 23:54:04 +0800

    update

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+$("#loginForm").submit(function(e){
+
+e.preventDefault();
+
+var username = $("#username").val();
+var password = $("#password").val();
+
+if(username == "" || password == "")
+{
+$("#popupMsg").popup("open");
+}
+else
+{
+$.mobile.changePage("#infoPage");
+}
+
+});
+
+});
